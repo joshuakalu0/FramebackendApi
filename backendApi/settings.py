d8ffa993e69c66a7e16fe45f1009ccc9efc77b50
joshuakaluO <ajujoshua2019@gmail.com>
2024-04-04 15:13:54 +0100

    new

--- backendApi/settings.py
+++ backendApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rg1xeq6h2r+8j!k)aroy#xl5bqahiatssy)=-7tvkvb_s1)c)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
